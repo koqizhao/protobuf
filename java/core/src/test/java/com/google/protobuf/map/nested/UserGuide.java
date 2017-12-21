01c8e35d8dc58557ff4564c943ba1f1affb7ff68
Qiang Zhao <koqizhao@outlook.com>
2017-12-21 14:32:54 +0800

    add UserGuide case

---
+++ java/core/src/test/java/com/google/protobuf/map/nested/UserGuide.java
@@ -0,0 +1,108 @@
+package com.google.protobuf.map.nested;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.protobuf.ProtobufMapSerializer;
+import com.google.protobuf.WireFormat.FieldType;
+import com.google.protobuf.map.nested.NestedMapDemoOuterClass.NestedMapDemo;
+
+/**
+ * @author koqizhao
+ *
+ *         Dec 21, 2017
+ */
+public class UserGuide {
+
+  @Test
+  public void nestedMapInMessage() throws IOException {
+    // init the nested map
+    Map<String, String> mapValue = new HashMap<String, String>();
+    mapValue.put("ok1", "ok1_value");
+    mapValue.put("ok2", "ok2_value");
+    Map<Integer, Map<String, String>> nestedMap = new HashMap<Integer, Map<String, String>>();
+    nestedMap.put(1, mapValue);
+
+    // init the message with nested map field
+    NestedMapDemo message = NestedMapDemo.newBuilder().setTitle("ok").setUrl("http://test").addSnippets("ok")
+        .putAllMetadata(nestedMap).build();
+    System.out.println(message);
+
+    // serialization
+    byte[] bytes = null;
+    ByteArrayOutputStream os = new ByteArrayOutputStream();
+    try {
+      message.writeTo(os);
+      bytes = os.toByteArray();
+    } finally {
+      os.close();
+    }
+
+    // deserialization
+    NestedMapDemo message2 = null;
+    ByteArrayInputStream is = new ByteArrayInputStream(bytes);
+    try {
+      message2 = NestedMapDemo.parseFrom(is);
+      System.out.println(message2);
+    } finally {
+      is.close();
+    }
+
+    // same as origin
+    Assert.assertEquals(message, message2);
+  }
+
+  @Test
+  public void genericMapSerialization() throws IOException {
+    // init a nested generic map
+    Map<Integer, Map<Integer, Integer>> map = new HashMap<Integer, Map<Integer, Integer>>();
+    map.put(11, new HashMap<Integer, Integer>());
+    map.get(11).put(12, 13);
+    map.put(21, new HashMap<Integer, Integer>());
+    map.get(21).put(22, 23);
+    map.get(21).put(24, 25);
+    System.out.println(map);
+
+    // init pb map serializer, which is thread safe & reusable
+    // nested map can be think as multiple keys & a single value:
+    //    key1, key2, ..., keyN, value
+    ProtobufMapSerializer<Integer, Map<Integer, Integer>> serializer = ProtobufMapSerializer
+        .<Integer, Map<Integer, Integer>> newBuilder().keyTypes(FieldType.INT32, FieldType.INT32)
+        .valueType(FieldType.INT32).build();
+
+    // Notice:
+    // the Builder has another method:
+    //    valueDefault(Object valueDefault)
+    // if valueType is Enum nor Message, valueDefault method must be invoked
+
+    // serialization
+    byte[] bytes = null;
+    ByteArrayOutputStream os = new ByteArrayOutputStream();
+    try {
+      serializer.serialize(os, map);
+      bytes = os.toByteArray();
+    } finally {
+      os.close();
+    }
+
+    // deserialization
+    Map<Integer, Map<Integer, Integer>> map2;
+    ByteArrayInputStream is = new ByteArrayInputStream(bytes);
+    try {
+      map2 = serializer.deserialize(is);
+      System.out.println(map2);
+    } finally {
+      is.close();
+    }
+
+    // same as origin
+    Assert.assertEquals(map, map2);
+  }
+
+}
